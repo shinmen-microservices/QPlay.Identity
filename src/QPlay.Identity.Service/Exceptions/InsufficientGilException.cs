4bc334b5e4005a1da5b2266196097a7bf480fd60
Shinmen Dokkodo <shinmen.dokkodo@gmail.com>
2023-07-12 15:56:25 +0530

    comments added

--- src/QPlay.Identity.Service/Exceptions/InsufficientGilException.cs
+++ src/QPlay.Identity.Service/Exceptions/InsufficientGilException.cs
@@ -2,9 +2,17 @@ using System;
 
 namespace QPlay.Identity.Service.Exceptions;
 
+/// <summary>
+/// Exception thrown when there is insufficient Gil (currency) to perform a debit transaction.
+/// </summary>
 [Serializable]
-internal class InsufficientGilException : Exception
+public class InsufficientGilException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InsufficientGilException"/> class with the specified user identifier and the amount of Gil to debit.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user.</param>
+    /// <param name="gilToDebit">The amount of Gil to debit.</param>
     public InsufficientGilException(Guid userId, decimal gilToDebit)
         : base($"Not enough gil to debit {gilToDebit} from user '{userId}'")
     {
@@ -12,7 +20,13 @@ internal class InsufficientGilException : Exception
         GilToDebit = gilToDebit;
     }
 
+    /// <summary>
+    /// Gets the unique identifier of the user.
+    /// </summary>
     public Guid UserId { get; }
 
+    /// <summary>
+    /// Gets the amount of Gil to debit.
+    /// </summary>
     public decimal GilToDebit { get; }
 }
--- src/QPlay.Identity.Service/Exceptions/UnknownUserException.cs
+++ src/QPlay.Identity.Service/Exceptions/UnknownUserException.cs
@@ -2,14 +2,24 @@ using System;
 
 namespace QPlay.Identity.Service.Exceptions;
 
+/// <summary>
+/// Exception thrown when an unknown user is encountered.
+/// </summary>
 [Serializable]
-internal class UnknownUserException : Exception
+public class UnknownUserException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UnknownUserException"/> class with the specified user identifier.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the unknown user.</param>
     public UnknownUserException(Guid userId)
         : base($"Unknown user '{userId}'")
     {
         UserId = userId;
     }
 
+    /// <summary>
+    /// Gets the unique identifier of the unknown user.
+    /// </summary>
     public Guid UserId { get; }
 }
