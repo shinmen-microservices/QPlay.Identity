b74d3f885295bc6760d74d0047fb3f58c10f3730
Shinmen Dokkodo <shinmen.dokkodo@gmail.com>
2023-07-12 14:33:02 +0530

    added masstransit publishendpoint to publish user update events

--- src/QPlay.Identity.Service/Controllers/UsersController.cs
+++ src/QPlay.Identity.Service/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using QPlay.Identity.Contracts;
 using QPlay.Identity.Service.Extensions;
 using QPlay.Identity.Service.Models.Dtos;
 using QPlay.Identity.Service.Models.Entities;
@@ -18,17 +19,34 @@ namespace QPlay.Identity.Service.Controllers;
 [Authorize(Policy = LocalApi.PolicyName)]
 public class UsersController : ControllerBase
 {
+    private readonly IPublishEndpoint publishEndpoint;
     private readonly UserManager<ApplicationUser> userManager;
 
-    public UsersController(UserManager<ApplicationUser> userManager)
+    public UsersController(
+        UserManager<ApplicationUser> userManager,
+        IPublishEndpoint publishEndpoint
+    )
     {
         this.userManager = userManager;
+        this.publishEndpoint = publishEndpoint;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute] Guid id)
+    {
+        ApplicationUser user = await userManager.FindByIdAsync(id.ToString());
+        if (user == null)
+            return NotFound();
+
+        await userManager.DeleteAsync(user);
+        await publishEndpoint.Publish(new UserUpdated(user.Id, user.Email, 0));
+        return NoContent();
     }
 
     [HttpGet]
     public ActionResult<IEnumerable<UserDto>> Get()
     {
-        IEnumerable<UserDto> users = userManager.Users.ToList().Select(user => user.AsDto());
+        IEnumerable<UserDto> users = userManager.Users.AsEnumerable().Select(user => user.AsDto());
         return Ok(users);
     }
 
@@ -36,7 +54,8 @@ public class UsersController : ControllerBase
     public async Task<ActionResult<UserDto>> GetByIdAsync([FromRoute] Guid id)
     {
         ApplicationUser user = await userManager.FindByIdAsync(id.ToString());
-        if (user == null) return NotFound();
+        if (user == null)
+            return NotFound();
 
         return user.AsDto();
     }
@@ -45,26 +64,15 @@ public class UsersController : ControllerBase
     public async Task<IActionResult> PutAsync([FromRoute] Guid id, [FromBody] UpdateUserDto userDto)
     {
         ApplicationUser user = await userManager.FindByIdAsync(id.ToString());
-        if (user == null) return NotFound();
+        if (user == null)
+            return NotFound();
 
         user.Email = userDto.Email;
         user.UserName = userDto.Email;
         user.Gil = userDto.Gil;
 
         await userManager.UpdateAsync(user);
-
-        return NoContent();
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteAsync([FromRoute] Guid id)
-    {
-        ApplicationUser user = await userManager.FindByIdAsync(id.ToString());
-        if (user == null) return NotFound();
-
-        await userManager.DeleteAsync(user);
-
+        await publishEndpoint.Publish(new UserUpdated(user.Id, user.Email, user.Gil));
         return NoContent();
     }
 }
-
