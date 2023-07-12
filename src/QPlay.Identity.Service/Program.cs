e6681e584e8a710098468e2227104fb05a2a60c0
Shinmen Dokkodo <shinmen.dokkodo@gmail.com>
2023-07-12 15:59:46 +0530

    added masstransit to identity service and minor identation corrections

--- src/QPlay.Identity.Service/Program.cs
+++ src/QPlay.Identity.Service/Program.cs
@@ -6,22 +6,17 @@ using QPlay.Identity.Service.HostedServices;
 
 namespace QPlay.Identity.Service;
 
-public class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-        
         // Add services to the container.
-
         builder.Services.ConfigureApplicationUser(builder.Configuration);
-        
         builder.Services.ConfigureIdentityServer(builder.Configuration);
-
+        builder.Services.ConfigureMassTransit();
         builder.Services.AddControllers();
-
         builder.Services.AddHostedService<IdentitySeedHostedService>();
-
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
@@ -37,17 +32,11 @@ public class Program
         }
 
         app.UseHttpsRedirection();
-
         app.UseStaticFiles();
-
         app.UseIdentityServer();
-
         app.UseAuthorization();
-
         app.MapControllers();
-
         app.MapRazorPages();
-
         app.Run();
     }
 }
